61d6c1d10b028113da1979e609aec1c89fb0d456
r0ld3x <77254818+r0ld3x@users.noreply.github.com>
2021-01-17 21:17:13 +0530

    Update broadcast.py

--- userbot/plugins/broadcast.py
+++ userbot/plugins/broadcast.py
@@ -1,15 +1,3 @@
-#    Copyright (C) Midhun KM 2020
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 
 import io
@@ -21,7 +9,7 @@ from userbot import CMD_HELP
 from userbot.utils import admin_cmd
 
 
-loggy_grp = Config.PRIVATE_GROUP_ID
+loggy_grp = Config.PM_PERMIT_GROUP_ID
 
 
 @bot.on(admin_cmd(pattern="badd ?(.*)"))
