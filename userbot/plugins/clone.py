49fb61290ba70ba158847a1d2eab02a19d1949f5
r0ld3x <77254818+r0ld3x@users.noreply.github.com>
2021-01-18 08:37:10 +0530

    Update clone.py

--- userbot/plugins/clone.py
+++ userbot/plugins/clone.py
@@ -15,9 +15,9 @@ from userbot import ALIVE_NAME, BIO_MSG, CMD_HELP
 from userbot.utils import admin_cmd
 from userbot.cmdhelp import CmdHelp
 
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Hell User"
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Ultron"
 
-DEFAULTUSERBIO = str(BIO_MSG) if BIO_MSG else "ʟɛɢɛռɖaʀʏ ᴀғ ɦɛʟʟɮօt"
+DEFAULTUSERBIO = str(BIO_MSG) if BIO_MSG else "Ultron Prime Is here"
 BOTLOG_CHATID = Config.PRIVATE_GROUP_BOT_API_ID
 BOTLOG = True
 
