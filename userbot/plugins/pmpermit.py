82b67c58441d8771d81a312b72cc16f60f91d59e
r0ld3x <77254818+r0ld3x@users.noreply.github.com>
2021-01-23 08:52:47 +0530

    Update pmpermit.py

--- userbot/plugins/pmpermit.py
+++ userbot/plugins/pmpermit.py
@@ -30,7 +30,7 @@ KRAKEN = (
     if CUSTOM_PMPERMIT
     else "**YOU HAVE TRESPASSED TO MY MASTERS INBOX** \n THIS IS ILLEGAL AND REGARDED AS CRIME"
 )
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Hêll Usêr"
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Ultron"
 USER_BOT_WARN_ZERO = "**You were spamming my sweet master's inbox, henceforth you have been blocked by my master's Ultron.**\n__Now GTFO, i'm busy__"
 USER_BOT_NO_WARN = (
     "Hello, This is **Ultron The Ultra Private Security Protocol⚠️**.\n"
